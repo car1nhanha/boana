62ee7b4624ac9396037148757aad7b01873b7617
car1nhanha <lucascarinhanha4@gmail.com>
2025-12-21 19:39:19 -0400

    feat: add Navbar component and landing page structure
    - Implemented NavbarComponent with navigation links for Services, Creators, and Documentation.
    - Created corresponding SCSS styles for the Navbar.
    - Developed LandingTemplate to wrap the landing page content, including the Navbar and footer.
    - Added LandingPage component with placeholder content and instructions for styling.
    - Established responsive design for the landing template and navbar.

---
+++ landing/src/components/pages/index.tsx
@@ -0,0 +1,10 @@
+import { LandingTemplate } from "../templates/landing";
+
+export const LandingPage = () => {
+  return (
+    <LandingTemplate>
+      <h1>Edite este arquivo para modificar a página inicial</h1>
+      <p>crie um arquivo de estilo .scss para esta página.</p>
+    </LandingTemplate>
+  );
+};
