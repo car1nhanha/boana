f4f5676a0dc2f529b0d0fca09d2d09d1690966f0
car1nhanha-sinapsys <lucas.araujo@sinapsystech.com.br>
2026-01-01 16:17:34 -0400

    feat: criação do header

--- landing/src/components/pages/index.tsx
+++ landing/src/components/pages/index.tsx
@@ -1,10 +1,28 @@
+import ilustration from "../../assets/illustration.png";
 import { LandingTemplate } from "../templates/landing";
+import styles from "./styles.module.scss";
 
 export const LandingPage = () => {
   return (
     <LandingTemplate>
-      <h1>Edite este arquivo para modificar a página inicial</h1>
-      <p>crie um arquivo de estilo .scss para esta página.</p>
+      <header className={styles.header}>
+        <div className={styles.headerLeft}>
+          <h1>
+            Virtual healthcare
+            <br />
+            for you
+          </h1>
+
+          <p>Trafalgar provides progressive, and affordable healthcare, accessible on mobile and online for everyone</p>
+
+          <div>
+            <button>Consult today</button>
+          </div>
+        </div>
+        <div className={styles.headerRight}>
+          <img src={ilustration} alt="header image" />
+        </div>
+      </header>
     </LandingTemplate>
   );
 };
